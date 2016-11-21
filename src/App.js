ccbcd1022d006cd22de51b1a0d8d7f5e7aab3811
Michele Memoli <michele@100shapes.com>
2016-11-21 17:45:38 +0000

    Added form to build project string

--- src/App.js
+++ src/App.js
@@ -4,7 +4,30 @@ import "materialize-css/dist/css/materialize.css";
 import "./App.css";
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      client_name: '',
+      project_name: ''
+    };
+  }
+
+  handleClientChange(client_name) {
+    this.setState({client_name});
+  }
+
+  handleProjectChange(project_name) {
+    this.setState({project_name});
+  }
+
   render() {
+
+    const {
+      client_name,
+      project_name
+    } = this.state;
+
     return (
       <div>
         <nav className="light-blue lighten-1" role="navigation">
@@ -25,16 +48,37 @@ class App extends Component {
               <form className="col s12">
                 <div className="row">
                   <div className="input-field col s6">
-                    <input placeholder={`e.g. "Burberry"`} id="client_name" type="text" className="validate"/>
+                    <input placeholder={`e.g. "Burberry"`}
+                           id="client_name"
+                           type="text"
+                           className="validate"
+                           onChange={(event) => {
+                             const {value} = event.target;
+                             this.handleClientChange(value);
+                           }}
+                           value={ client_name }/>
                     <label htmlFor="client_name">Client Name</label>
                   </div>
                   <div className="input-field col s6">
-                    <input placeholder={`e.g. "Transparency"`} id="project_name" type="text" className="validate"/>
+                    <input placeholder={`e.g. "Transparency"`}
+                           id="project_name"
+                           type="text"
+                           className="validate"
+                           onChange={(event) => {
+                             const {value} = event.target;
+                             this.handleProjectChange(value);
+                           }}
+                           value={ project_name }/>
                     <label htmlFor="project_name">Project Name</label>
                   </div>
                 </div>
               </form>
             </div>
+          </div>
+          <div className="section">
+            <h5>
+              Project : { this.state.client_name } : { this.state.project_name }
+            </h5>
           </div>
         </div>
       </div>
